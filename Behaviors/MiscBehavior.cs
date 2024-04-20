9a6c9c2dce2a0ce69cded3f75f3620fb5d6a6729
Cephas369 <pedro-leitao@outlook.com>
2024-04-20 15:12:13 -0400

    Refactor

--- Behaviors/MiscBehavior.cs
+++ Behaviors/MiscBehavior.cs
@@ -11,31 +11,15 @@ namespace BasicOverhaul.Behaviors;
 
 internal class MiscBehavior : CampaignBehaviorBase
 {
-    private InputKey _fastForwardKey = InputKey.Numpad9;
     public override void RegisterEvents()
     {
         CampaignEvents.OnGameLoadFinishedEvent.AddNonSerializedListener(this, OnGameLoadFinished);
-        CampaignEvents.MissionTickEvent.AddNonSerializedListener(this, OnTick);
-    }
-
-    private void OnTick(float dt)
-    {
-        if (Input.IsKeyReleased(_fastForwardKey))
-        {
-            if (Mission.Current == null)
-                return;
-            
-            Mission.Current.SetFastForwardingFromUI(!Mission.Current.IsFastForward);
-        }
     }
 
     private void OnGameLoadFinished()
     {
         if (BasicOverhaulCampaignConfig.Instance != null)
             AccessTools.Property(typeof(NativeConfig), "CheatMode").SetValue(null, BasicOverhaulCampaignConfig.Instance.CheatModeEnabled);
-
-        _fastForwardKey = (InputKey)Enum.Parse(typeof(InputKey),
-            BasicOverhaulGlobalConfig.Instance?.FastForwardMissionKey?.SelectedValue ?? "Numpad9");
     }
     
     public override void SyncData(IDataStore dataStore)
@@ -46,6 +30,7 @@ internal class MiscBehavior : CampaignBehaviorBase
 
 internal class MiscMissionLogic : MissionLogic
 {
+    private InputKey _fastForwardKey = InputKey.Numpad9;
     public override void OnAgentHit(Agent affectedAgent, Agent affectorAgent, in MissionWeapon affectorWeapon, in Blow blow,
         in AttackCollisionData attackCollisionData)
     {
@@ -54,5 +39,24 @@ internal class MiscMissionLogic : MissionLogic
             affectedAgent.Health = affectedAgent.HealthLimit;
         else if (MissionOptions.MountInvincible && affectedAgent.IsMount && affectedAgent.RiderAgent?.IsMainAgent == true)
             affectedAgent.Health = affectedAgent.HealthLimit;
+    }
+
+    public override void OnCreated()
+    {
+        base.OnCreated();
+        _fastForwardKey = (InputKey)Enum.Parse(typeof(InputKey),
+            BasicOverhaulGlobalConfig.Instance?.FastForwardMissionKey?.SelectedValue ?? "Numpad9");
+    }
+
+    public override void OnMissionTick(float dt)
+    {
+        base.OnMissionTick(dt);
+        if (Input.IsKeyReleased(_fastForwardKey))
+        {
+            if (Mission.Current == null)
+                return;
+            
+            Mission.Current.SetFastForwardingFromUI(!Mission.Current.IsFastForward);
+        }
     }
 }
